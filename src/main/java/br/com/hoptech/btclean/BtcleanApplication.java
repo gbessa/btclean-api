baaccaddfcdce224eeb65822c979b9a534f3a26e
Gustavo Bessa <gbvirtual@gmail.com>
2020-09-28 15:39:53 +0100

    Image Readme

---
+++ src/main/java/br/com/hoptech/btclean/BtcleanApplication.java
@@ -0,0 +1,13 @@
+package br.com.hoptech.btclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hoptech/btclean/BtcleanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hoptech.btclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
